18507f20875b84f800c39783763153f125488139
khang <chaoskid1415@gmail.com>
2025-06-21 14:47:39 +0700

    Xây dựng dự án Slidezy

---
+++ PJ4_Slidezy/src/slidezy.js
@@ -0,0 +1,58 @@
+function Slidezy(selector, options = {}) {
+    if (typeof selector !== "string") {
+        throw new Error("Selector must be a string");
+    }
+    this.container = document.querySelector(selector);
+
+    if (!this.container) {
+        throw new Error(`Element not found for selector: ${selector}`);
+    }
+
+    this.opt = Object.assign({}, options);
+    this._slides = Array.from(this.container.children);
+
+    this._originalHTML = this.container.innerHTML;
+
+    this._init();
+}
+
+Slidezy.prototype._init = function () {
+    this.container.classList.add("slidezy-wrapper");
+
+    this._createTrack();
+    this._createNavigation();
+};
+
+Slidezy.prototype._createTrack = function () {
+    this.track = document.createElement("div");
+    this.track.classList.add("slidezy-track");
+
+    this._slides.forEach((slide) => {
+        slide.classList.add("slidezy-slide");
+        this.track.appendChild(slide);
+    });
+
+    this.container.append(this.track);
+};
+
+Slidezy.prototype._createNavigation = function () {
+    this.btnPrev = document.createElement("button");
+    this.btnNext = document.createElement("button");
+
+    this.btnPrev.textContent = "<";
+    this.btnNext.textContent = ">";
+
+    this.btnNext.classList.add("slidezy-next");
+    this.btnPrev.classList.add("slidezy-prev");
+
+    this.container.append(this.btnPrev, this.btnNext);
+
+    this.btnNext.onclick = () => {
+        this._moveSlice(1);
+    };
+    this.btnPrev.onclick = () => {
+        this._moveSlice(-1);
+    };
+};
+
+Slidezy.prototype._moveSlice = function (direction) {};
